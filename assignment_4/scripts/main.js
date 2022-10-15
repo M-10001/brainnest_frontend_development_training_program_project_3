9933180960c9c4a1a4149102f9d8e5337f8629c3
Mman1mm <67879252+Mman1mm@users.noreply.github.com>
2022-10-15 16:09:41 +0100

    Separate into functions.

--- assignment_4/scripts/main.js
+++ assignment_4/scripts/main.js
@@ -1,5 +1,17 @@
 const CHOICES = ["rock", "paper", "scissor"]
 const TOTAL_GAME_ROUNDS = 5;
+let playerScore = 0;
+let computerScore = 0;
+
+function getPlayerInput() {
+  let playerSelection = prompt(`Input choice from [${CHOICES}] to play:`).toLowerCase();
+
+  while (!CHOICES.includes(playerSelection)) {
+    playerSelection = prompt(`Wrong input.\nInput choice again from [${CHOICES}] to play:`).toLowerCase();
+  }
+
+  return playerSelection;
+}
 
 function computerPlay() {
   return CHOICES[Math.floor(Math.random() * CHOICES.length)];
@@ -19,42 +31,22 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-
-  for (let i = 0; i < TOTAL_GAME_ROUNDS; i++) {
-    let playerSelection = prompt(`Input choice from [${CHOICES}] to play:`).toLowerCase();
-
-    while (!CHOICES.includes(playerSelection)) {
-      playerSelection = prompt(`Wrong input.\nInput choice again from [${CHOICES}] to play:`).toLowerCase();
-    }
-
-    let computerSelection = computerPlay();
-    console.log();
-    console.log(`Player chose: ${playerSelection}`);
-    console.log(`Computer chose: ${computerSelection}`)
-    console.log();
-    let roundOutput = playRound(playerSelection, computerSelection);
-
-    switch (roundOutput) {
-      case "draw":
-        console.log(`Round ${i + 1}: Draw!`);
-        break;
-      case "player":
-        playerScore++;
-        console.log(`Round ${i + 1}: The player wins this round!`);
-        break;
-      default:
-        computerScore++;
-        console.log(`Round ${i + 1}: The computer wins this round!`)
-    }
-
-    console.log();
-    console.log("----------");
-    console.log();
+function outputRoundResult(roundOutput, currentRound) {
+  switch (roundOutput) {
+    case "draw":
+      console.log(`Round ${currentRound}: Draw!`);
+      break;
+    case "player":
+      playerScore++;
+      console.log(`Round ${currentRound}: The player wins this round!`);
+      break;
+    default:
+      computerScore++;
+      console.log(`Round ${currentRound}: The computer wins this round!`)
   }
+}
 
+function outputFinalResult(playerScore, computerScore) {
   console.log(`Player final score: ${playerScore}`);
   console.log(`Computer final score: ${computerScore}`);
 
@@ -65,6 +57,27 @@ function game() {
   } else {
     console.log("The computer wins!");
   }
+}
+
+function game() {
+  playerScore = 0;
+  computerScore = 0;
+
+  for (let i = 0; i < TOTAL_GAME_ROUNDS; i++) {
+    let playerSelection = getPlayerInput();
+    let computerSelection = computerPlay();
+    console.log();
+    console.log(`Player chose: ${playerSelection}`);
+    console.log(`Computer chose: ${computerSelection}`)
+    console.log();
+    let roundOutput = playRound(playerSelection, computerSelection);
+    outputRoundResult(roundOutput, i + 1);
+    console.log();
+    console.log("----------");
+    console.log();
+  }
+
+  outputFinalResult(playerScore, computerScore);
 }
 
 game();
