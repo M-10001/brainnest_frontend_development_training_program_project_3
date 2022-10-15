4d57f2b18b9d23ead6226c2a0cf9085876baff2d
Mman1mm <67879252+Mman1mm@users.noreply.github.com>
2022-10-15 03:37:00 +0100

    Complete assignment 4.

---
+++ assignment_4/scripts/main.js
@@ -0,0 +1,48 @@
+const CHOICES = ["rock", "paper", "scissor"]
+
+function computerPlay() {
+  return CHOICES[Math.floor(Math.random() * CHOICES.length)];
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "draw";
+  } else if ((playerSelection === CHOICES[0]) && (computerSelection === CHOICES[2])) {
+    return "player";
+  } else if ((playerSelection === CHOICES[1]) && (computerSelection === CHOICES[0])) {
+    return "player";
+  } else if ((playerSelection === CHOICES[2]) && (computerSelection === CHOICES[1])) {
+    return "player";
+  } else {
+    return "computer";
+  }
+}
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt(`Input choice from [${CHOICES}]:`).toLowerCase();
+
+    while (!CHOICES.includes(playerSelection)) {
+      playerSelection = prompt(`Wrong input. Input choice again from [${CHOICES}]:`).toLowerCase();
+    }
+
+    let computerSelection = computerPlay();
+    console.log();
+    console.log(`Player chose: ${playerSelection}`);
+    console.log(`Computer chose: ${computerSelection}`)
+    console.log();
+    let gameOutput = playRound(playerSelection, computerSelection);
+
+    if (gameOutput === "draw"){
+      console.log("Output: Draw!");
+    } else {
+      console.log(`Ouput: The ${gameOutput} wins!`)
+    }
+
+    console.log();
+    console.log("----------");
+    console.log();
+  }
+}
+
+game();
